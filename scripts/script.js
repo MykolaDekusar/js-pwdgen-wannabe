f0919bb1798a8b2b2931ce406f261f49f5c1ed86
MykolaDekusar <ndekusar97@gmail.com>
2024-04-23 14:35:02 +0200

    [FIX]Improved code with added comments and explanation

--- scripts/script.js
+++ scripts/script.js
@@ -10,23 +10,24 @@ Bonus 1: visualizzate le informazioni anche nella console del browser;
 Bonus 2: provate a generare un numero casuale con Javascript;
 */
 
-let currentElement = document.getElementById("password");
-let userPassword = currentElement.innerHTML;
-const firstName = prompt("Inserisci il tuo nome");
-const secondName = prompt("Inserisci il tuo cognome");
-const favColor = prompt("Inserisci il tuo colore preferito");
-const randomNumber = Math.floor((Math.random() * 10000) + 1);
+const currentElement = document.getElementById("password"); //assegno un valore costante preso dall'elemento span dentro index.html
+const firstName = prompt("Inserisci il tuo nome"); //chiedo il nome e lo assegno alla costante firstName 
+const secondName = prompt("Inserisci il tuo cognome"); //chiedo il cognome e lo assegno alla costante secondName
+const favColor = prompt("Inserisci il tuo colore preferito"); //chiedo il colore preferito e lo assegno alla costante favColor
+const randomNumber = Math.floor((Math.random() * 10000) + 1); //creo una costante di numero casuale che varia da 1 a 10000 inclusi
+let userPassword = currentElement.innerHTML;  //assegno un valore variabile in formato stringa
 
 
-//main exercise
+//main exercise    assegno alla stringa userPassword un insieme di stringhe
 //userPassword = `${userPassword}${firstName}${secondName}${favColor}21`;
 
-//BONUS 2
+//BONUS 2                     //sostituisco il 21 con la costante del numero casuale
 userPassword = `${userPassword}${firstName}${secondName}${favColor}${randomNumber}`;
 
-//BONUS 1
+//BONUS 1 riporto nella console la stringa password = e la stringa composta di userPassword
 console.log("password = " + userPassword);
 
+//vado a modificare l'html interno del tag che ha l'id:password
 currentElement.innerHTML = userPassword;
 
 
